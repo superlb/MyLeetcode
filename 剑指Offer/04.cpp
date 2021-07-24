ae617c7e858636b026ba3c323f37972dda64b87e
superlb <liaowuyao@gmail.com>
2021-07-24 22:03:05 +0800

    2021秋day2

--- 剑指Offer/04.cpp
+++ 剑指Offer/04.cpp
@@ -22,3 +22,21 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0) return false;
+        int i=0, j=matrix[0].size() - 1;
+        while(i < matrix.size() && j >=0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                ++i;
+            } else if (matrix[i][j] > target) {
+                --j;
+            }
+        }
+        return false;
+    }
+};
--- 剑指Offer/05.cpp
+++ 剑指Offer/05.cpp
@@ -12,3 +12,18 @@ public:
         return str;
     }
 };
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string str;
+        for(auto a : s) {
+            if (a == ' ') {
+                str += "%20";
+            } else {
+                str += a;
+            }
+        }
+        return str;
+    }
+};
